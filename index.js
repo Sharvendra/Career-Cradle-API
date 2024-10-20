fb2810f658f3b34ecab1fc7fd768f88793407b6a
Sharvendra <sharvendra2003@gmail.com>
2024-10-20 12:58:42 +0530

    Job controller and job route created and tested

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
+import job from "./routes/job.route.js";
 
 dotenv.config({});
 
@@ -33,7 +34,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-
+app.use("/api/v1/job", job);
 // "http://localhost:8000/api/v1/user/register" 
 
 app.listen(PORT, ()=>{
